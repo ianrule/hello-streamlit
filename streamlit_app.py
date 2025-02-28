06ce1df860594f8fa128d6d98de05df31232fda8
Ian Rule <53279635+ianrule@users.noreply.github.com>
2025-02-28 22:20:25 +0000

    changing app title

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("Ian's Streamlit Test App")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Refer to [docs.streamlit.io](https://docs.streamlit.io/) for further development."
 )
